4f2213555e2e673a3c73b2400e8d24c7c8a12354
Steven Stojanovski <steven.stojanovski@gmail.com>
2017-08-09 18:58:58 +1000

    Update proxmark3.rb

--- proxmark3.rb
+++ proxmark3.rb
@@ -3,7 +3,7 @@ class Proxmark3 < Formula
   homepage "http://www.proxmark.org"
   url "https://github.com/stoja/proxmark3/archive/master.zip"
   sha256 "7571a50af0ab58853f037649de0afab4161022088d459e8b1e5c567e31a8d241"
-  head "https://github.com/Proxmark/proxmark3.git"
+  head "https://github.com/stoja/proxmark3.git"
 
   depends_on "automake" => :build
   depends_on "readline"
